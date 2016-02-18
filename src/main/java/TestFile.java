269328d381d836e99d7a5b3b22cc937ff41d1f1b
Marcin Smialek <msm@touk.pl>
2016-02-18 17:29:04 +0100

    Reference project showing how to configure Sputnik on Github with Travis CI

---
+++ src/main/java/TestFile.java
@@ -0,0 +1,35 @@
+class TestFile {
+    public String foo() {
+        return "bar";
+    }
+
+    private static void incorrectAssignmentInIfCondition() {
+        boolean value = false;
+        if (value = false) {
+            //do Something
+        } else {
+            //else Do Something
+        }
+    }
+
+    private static void incorrectComparingToItself() {
+        int x = 3421;
+        if (x <= x) {
+            // whatever
+        }
+    }
+
+    private static void anotherVariationOfIncorrectComparingToItself() {
+        int x = 567;
+        if (x == x) {
+            // whatever
+        }
+    }
+
+    private static void onMoreVariationOfIncorrectComparingToItself() {
+        int x = 23422;
+        if (x >= x) {
+            // whatever
+        }
+    }
+}
